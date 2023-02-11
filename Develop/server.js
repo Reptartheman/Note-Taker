5eaeb3aebbe54e3774cd371bef469be0f0395474
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-02-11 14:29:41 -0500

    Made helpers folder

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,29 @@
 const express = require('express');
+const uuid = require('./helpers/uuid');
 const dbData = require('./db/db.json');
 const app = express();
 const PORT = 3001;
 
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
 
-
-
-
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
   });
 
-  app.get('/api/notes', (req, res) => res.json(dbData));
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+  });
+
+app.get('/api/notes', (req, res) => res.json(dbData));
+
+app.post()
+
 
 
 app.listen(PORT, () => {
