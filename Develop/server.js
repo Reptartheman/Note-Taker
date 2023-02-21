3b456bdc51f24669537312bbf65290a428768694
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-02-21 11:09:04 -0500

    added new routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,31 +1,71 @@
 const express = require('express');
-const uuid = require('./helpers/uuid');
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 const dbData = require('./db/db.json');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 
 
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-  });
+app.get('/api/notes', (req, res) => {
+  res.json(dbData)
+})
 
+app.post('api/notes', (req, res) => {
+  fs.readFile('./db/db.json', (err,data) => {
+    if (err) throw err;
+    const newData = JSON.parse(data);
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uuidv4()
+    }
+    newData.push(newNote);
+    fs.writeFile('./db/db.json', JSON.stringify(newData), (err) => {
+      if (err) throw err;
+    })
+  })
+})
+
+//Path to notes 
   app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
   });
 
-app.get('/api/notes', (req, res) => res.json(dbData));
 
-app.post()
-
+//Path to index
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
-  });
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
+
+/* GIVEN a note-taking application
+WHEN I open the Note Taker
+THEN I am presented with a landing page with a link to a notes page DONE
+
+WHEN I click on the link to the notes page
+THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+
+WHEN I enter a new note title and the note’s text
+THEN a Save icon appears in the navigation at the top of the page
+
+WHEN I click on the Save icon
+THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+
+WHEN I click on an existing note in the list in the left-hand column
+THEN that note appears in the right-hand column
+
+WHEN I click on the Write icon in the navigation at the top of the page
+THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+ */
