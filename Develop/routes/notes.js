ec203e9b9aa7c41a5c112c36ab423f86ee1e663c
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-02-25 11:44:31 -0500

    New note posted to db.json code added. This is a W for sure.

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -2,7 +2,7 @@ const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const dbData = require('../db/db.json');
-const fs = require('fs');
+
 
 
 
@@ -12,17 +12,22 @@ notes.get('/', (req, res) => {
 
 
     notes.post('/', (req, res) => {
-    readFromFile('./db/db.json', (err,data) => {
-      if (err) throw err;
-      const newData = JSON.parse(data);
-      const newNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uuidv4()
+      console.log(req.body);
+
+      const {title , text, note_id} = req.body;
+
+      if(req.body){
+        const newNote ={
+          title,
+          text,
+          note_id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json' );
+        res.json('YAY! You added a new note!')
+      } else {
+        res.error('Booooo no new note because of an error!');
       }
-      newData.push(newNote);
-      readAndAppend('./db/db.json', JSON.stringify(newData), (err) => {
-        if (err) throw err;
-      })
-    })
-  })
+  });
+
+  module.exports = notes;
