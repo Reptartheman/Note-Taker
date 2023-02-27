2a0186050fbed8c107041580c0322174477913cd
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-02-26 19:01:05 -0500

    commit with updated delete path

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,29 +1,16 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
-const dbData = require('../db/db.json');
-const fs = require('fs');
 
 
 //this route retrieves all of the notes
 notes.get('/', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
-});
-
-
-notes.get('/:id', (req, res) => {
-  for(let i = 0; i < dbData.length; i++){
-    if(dbData[i].id == req.params.id){
-     return res.json(dbData[i]);
-
-    };
-  };
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
   
 
 // POST route for a new note
     notes.post('/', (req, res) => {
-      console.log(req.body);
 
       const { title , text, } = req.body;
 
@@ -31,39 +18,28 @@ notes.get('/:id', (req, res) => {
         const newNote ={
           title,
           text,
-          note_id: uuidv4(),
+          id: uuidv4(),
         };
 
         readAndAppend(newNote, './db/db.json' );
 
-        const jsonResponse = {
-          status: 'success',
-          body: 'YAY! You added a new note!'
-        }
-
-        res.json(jsonResponse);
+        res.json('Note created!');
       } else {
-        res.error('Booooo no new note because of an error!');
+        res.error('Error creating note');
       }
   });
 
   //delete note path
   notes.delete('/:id', (req,res) => {
-    const id = req.params.id;
-
-
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err){
-        console.error(err);
-      } else {
-        const parsedData = JSON.parse(data);
-        const noteIndex = parsedData.findIndex(note => note.id == id);
-        parsedData.splice(noteIndex, 1);
-        writeToFile('./db/db.json', parsedData)
-        res.send(`${id} has been erased.`)
-      };
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data)) 
+    .then((notes) => {
+      console.log(res.json);
+      let previousNotes = notes.filter((note) => note.id !== noteId);
+      writeToFile('./db/db.json', previousNotes);
+      res.json('Note deleted!');
     });
-
 
   });
 
